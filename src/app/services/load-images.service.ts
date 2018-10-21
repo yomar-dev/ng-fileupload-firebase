af2c602f5100b344007f6ec926ea86b4046c39dc
Yomar Miranda <mirandayomar@gmail.com>
2018-10-21 15:09:39 -0500

    Upload images to firebase and save them in storage.

--- src/app/services/load-images.service.ts
+++ src/app/services/load-images.service.ts
@@ -16,6 +16,23 @@ export class LoadImagesService {
   }
 
   loadImagesToFirebase(images: FileItem[]) {
-    console.log(images);
+    const storageRef = firebase.storage().ref();
+
+    for (const item of images) {
+      if (item.progress >= 100) { continue; }
+
+      const uploadTask: firebase.storage.UploadTask = storageRef.child(`${ this.FOLDER_IMAGES }/${ item.fileName }`).put(item.file);
+      uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+        (snapshot: firebase.storage.UploadTaskSnapshot) => item.progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100,
+        (error) => console.error('Error to upload', error),
+        () => {
+          uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+            item.url = downloadURL;
+            item.isLoading = false;
+            this.saveImage({ name: item.fileName, url: item.url });
+          });
+        }
+      );
+    }
   }
 }
